3e2392970eafaaf50c0174c92790a8d32350f718
pparksso0308 <pparksso0308@gmail.com>
2018-10-11 20:39:00 +0900

    1.0

---
+++ 4-2/src/Grade.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Grade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int math;
+		int science;
+		int english;
+		double avg;
+		
+		public Grade(int _math, int _science, int _english)
+		{
+			math=_math;
+			science= _science;
+			english = _english;
+		}
+		
+		int average()
+		{
+			avg = (math+science+english)/3;
+			
+			return avg;
+		}
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("수학, 과학, 영어 순으로 3개의 점수 입력>>");
+		int math = scanner.nextInt();
+		int science = scanner.nextInt();
+		int english = scanner.nextInt();
+		
+		Grade me = new Grade(math,science,english);
+		System.out.println("평균은 "+me.average()); //average는 평균을 계상하여 리턴
+		
+		scanner.close();
+	}
+
+}
