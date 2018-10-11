1eec8b7b2e9eb7098d846779e28d0c9ddc48b062
pparksso0308 <pparksso0308@gmail.com>
2018-10-11 21:00:44 +0900

    1.2

--- 4-2/src/Grade.java
+++ 4-2/src/Grade.java
@@ -1,34 +1,34 @@
+
 import java.util.Scanner;
 
 public class Grade {
 
+		private int math;
+		private int science;
+		private int english;
+		double avg;
+
+	public Grade(int _math, int _science, int _english)
+	{
+		math=_math;
+		science= _science;
+		english = _english;
+	}
+	
+	public double average()
+	{
+		avg = (math+science+english)/3;
+		
+		return avg;
+	}
+	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		// TODO Auto-generated method stub
-		int math;
-		int science;
-		int english;
-		double avg;
+	
 		
-		
-		public Grade(int _math, int _science, int _english)
-		{
-			math=_math;
-			science= _science;
-			english = _english;
-		}
-		
-		int average()
-		{
-			avg = (math+science+english)/3;
-			
-			return avg;
-		}
-		
-		
-		
-		
-		System.out.println("수학, 과학, 영어 순으로 3개의 점수 입력>>");
+
+		System.out.print("수학, 과학, 영어 순으로 3개의 점수 입력>>");
 		int math = scanner.nextInt();
 		int science = scanner.nextInt();
 		int english = scanner.nextInt();
